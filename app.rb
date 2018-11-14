ed55eaf73dc5d94604cceaa1f1980b27872dcfa8
Cristopher Palacios <criszelaya24@hotmail.com>
2018-11-14 15:12:23 +0000

    Creating a class for initialize players

--- app.rb
+++ app.rb
@@ -8,20 +8,20 @@ class Battle < Sinatra::Base
 		erb :index
 	end
 	post '/names' do # Receiving values from the form that is in the index and then sending to :play that is NOW a get method below
-		session[:player_1_name] = params[:player_1_name]
-		session[:player_2_name] = params[:player_2_name]
+		$player_1 = Player.new(params[:player_1_name])
+		$player_2 = Player.new(params[:player_2_name])
 		redirect '/play'
 	end
 
 	get '/play' do
-		@player_1_name = session[:player_1_name]
-		@player_2_name = session[:player_2_name]
+		@player_1_name = $player_1.name
+		@player_2_name = $player_2.name
 		erb :play
 	end
 
 	get '/attack' do
-		@player_1_name = session[:player_1_name]
-		@player_2_name = session[:player_2_name]
+		@player_1_name = $player_1.name
+		@player_2_name = $player_2.name
 		erb :attack
 	end
 	run! if app_file == $0
