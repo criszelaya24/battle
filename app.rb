f9afd96b60158131aa843aff6f4ebfe3f48743d8
Cristopher Palacios <criszelaya24@hotmail.com>
2018-11-14 13:47:44 +0000

    Solving first user history with rspec

--- app.rb
+++ app.rb
@@ -4,6 +4,12 @@ require 'sinatra/base'
 class Battle < Sinatra::Base
 	get '/' do
 		'Testing infraestructure working'
+		erb :index
+	end
+	post '/names' do
+		@player_1_name = params[:player_1_name]
+		@player_2_name = params[:player_2_name]
+		erb :play
 	end
 	run! if app_file == $0
 end
--- spec/features/testing_infrastructure_spec.rb
+++
@@ -1,6 +0,0 @@
-feature 'Testing infraestructure' do
-	scenario 'can run app and check page content' do
-		visit('/')
-		expect(page).to have_content 'Testing infraestructure working'
-	end
-end
