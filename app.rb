1c43d81313adc8c2ab6ea1a44c973062e6cff617
Cristopher Palacios <criszelaya24@hotmail.com>
2018-11-14 14:23:28 +0000

    Initializing session to the user and redirect our method post to a get method

--- app.rb
+++ app.rb
@@ -2,14 +2,21 @@ require 'sinatra/base'
 #require 'shotgun'
 # shotgun app.rb -p 4567 // Once is installed sinatra with: gem install shotgun // run the following command and giving the default port
 class Battle < Sinatra::Base
+	enable :sessions
+
 	get '/' do
-		'Testing infraestructure working'
 		erb :index
 	end
-	post '/names' do
-		@player_1_name = params[:player_1_name]
-		@player_2_name = params[:player_2_name]
-		erb :play
+	post '/names' do # Receiving values from the form that is in the index and then sending to :play that is NOW a get method below
+		session[:player_1_name] = params[:player_1_name]
+		session[:player_2_name] = params[:player_2_name]
+		redirect '/play'
+	end
+
+	get '/play' do
+	  @player_1_name = session[:player_1_name]
+	  @player_2_name = session[:player_2_name]
+	  erb :play
 	end
 	run! if app_file == $0
 end
