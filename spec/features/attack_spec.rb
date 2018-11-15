8c2c5e6efe6a92971b9b8d9a1daa6814309ef4a4
Cristopher Palacios <criszelaya24@hotmail.com>
2018-11-15 11:40:43 +0000

    Correcting how to show the hit point correctly

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -9,7 +9,9 @@ end
 feature '#Attacking' do
 	scenario 'Reduce player 2 HP by 10' do
 		sign_in_and_play
-		click_link 'Attack'
+		click_link 'Attack'	
+		click_link 'OK'
+		expect(page).not_to have_content 'Mittens: 60HP'
 		expect(page).to have_content 'Mittens: 50HP'
 	end
 end
