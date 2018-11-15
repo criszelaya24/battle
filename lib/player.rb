9e87b419e41ef1e3b03561ada12e0291b4f100e5
Cristopher Palacios <criszelaya24@hotmail.com>
2018-11-15 10:39:51 +0000

    Reducing HP from player 2 and showing results

--- lib/player.rb
+++ lib/player.rb
@@ -1,6 +1,16 @@
 class Player
-	attr_accessor :name
-	def initialize(name)
+	attr_accessor :name, :hit_points
+	DEFAULT_HIT_POINTS = 60
+	def initialize(name, hit_points = DEFAULT_HIT_POINTS)
 		@name = name
+		@hit_points = hit_points
+	end
+
+	def attack(player)
+		player.receive_damage
+	end
+
+	def receive_damage
+		@hit_points -= 10
 	end
 end
--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -5,3 +5,11 @@ feature 'Attacking to the user 2' do
 		expect(page).to have_content 'Dave attacked Mittens'
 	end 
 end
+
+feature '#Attacking' do
+	scenario 'Reduce player 2 HP by 10' do
+		sign_in_and_play
+		click_link 'Attack'
+		expect(page).to have_content 'Mittens: 50HP'
+	end
+end
