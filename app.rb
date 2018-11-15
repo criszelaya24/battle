40bce2e17fd407b9ebff9b35cfebb9e312f1765b
Cristopher Palacios <criszelaya24@hotmail.com>
2018-11-15 14:11:44 +0000

    Giving more responsability to the Game class

--- app.rb
+++ app.rb
@@ -10,21 +10,27 @@ class Battle < Sinatra::Base
 		erb :index
 	end
 	post '/names' do # Receiving values from the form that is in the index and then sending to :play that is NOW a get method below
-		$player_1 = Player.new(params[:player_1_name])
-		$player_2 = Player.new(params[:player_2_name])
+		# $player_1 = Player.new(params[:player_1_name])
+		# $player_2 = Player.new(params[:player_2_name])
+		player_1 = Player.new(params[:player_1_name])
+		player_2 = Player.new(params[:player_2_name])
+		$game = Game.new(player_1, player_2)
 		redirect '/play'
 	end
 
 	get '/play' do
-		@player_1 = $player_1
-		@player_2 = $player_2
+		# @player_1 = $player_1
+		# @player_2 = $player_2
+		@game = $game
 		erb :play
 	end
 
 	get '/attack' do
-		@player_1 = $player_1
-		@player_2 = $player_2
-		Game.new.attack(@player_2)
+		# @player_1 = $player_1
+		# @player_2 = $player_2
+		# Game.new.attack(@player_2)
+		@game = $game
+		@game.attack(@game.player_2)
 		erb :attack
 	end
 	run! if app_file == $0
